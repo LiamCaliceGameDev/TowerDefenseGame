a45eaa7ad2b74bd7d3469c3ed6b69b5850fcb637
Liam Calice <liamcalicegamedev@gmail.com>
2016-11-01 19:31:30 +0100

    Turret Shooting

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class Turret : MonoBehaviour {
 
 	private Transform target;
-	public float range = 15f;
 
+	[Header("Attributes")]
+	public float range = 15f;
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
+
+
+	[Header("Unity Setup Fields")]
 	public string enemyTag = "Enemy";
 
 	public Transform partToRotate;
 	public float turnSpeed = 10f;
+
+	public GameObject bulletPrefab;
+	public Transform firePoint;
 
 
 	void Start () {
@@ -46,10 +55,25 @@ public class Turret : MonoBehaviour {
 
 		Vector3 dir = target.position - transform.position;
 		Quaternion lookRotation = Quaternion.LookRotation (dir);
-		Vector3 rotation = Quaternion.Lerp (partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		Vector3 rotation =  Quaternion.Lerp (partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
 
+		if (fireCountdown <= 0f) {
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
 
+		fireCountdown -= Time.deltaTime;
+
+	}
+
+	void Shoot () {
+		GameObject bulletG0 = Instantiate (bulletPrefab, firePoint.position, firePoint.rotation) as GameObject;
+		Bullet bullet = bulletG0.GetComponent <Bullet> ();
+
+		if (bullet != null) {
+			bullet.Seek (target);
+		}
 	}
 
 	void OnDrawGizmosSelected () {
--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 public class Waypoints : MonoBehaviour {
-
+	
 	public static Transform[] points;
 
 	void Awake () {
