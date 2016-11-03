07946dc4f579bd8020c36677da32bc6fdea2369f
Liam Calice <liamcalicegamedev@gmail.com>
2016-11-03 18:29:28 +0100

    Shop To Build Turret

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	public static BuildManager instance;
+
+	void Awake () {
+		if (instance != null) {
+			Debug.LogError ("More Than One BuildManager In Scene!");
+			return;
+		}
+		instance = this;
+	}
+
+	public GameObject standardTurretPrefab;
+	public GameObject anotherTurretPrefab;
+
+
+	private GameObject turretToBuild;
+
+	public GameObject GetTurretToBuild () {
+		return turretToBuild;
+	}
+
+	public void SetTurretToBuild (GameObject turret) {
+		turretToBuild = turret;
+	}
+
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour {
+
+	public Color hoverColor;
+	public Vector3 positionOffset;
+
+	private GameObject turret;
+
+	private Renderer rend;
+	private Color startColor;
+
+	BuildManager buildManager;
+
+	void Start () {
+		rend = GetComponent <Renderer> ();
+		startColor = rend.material.color;
+		buildManager = BuildManager.instance;
+	}
+
+	void OnMouseDown () {
+		if (EventSystem.current.IsPointerOverGameObject ()) {
+			return;
+		}
+		if (buildManager.GetTurretToBuild () == null) {
+			return;
+		}
+
+		if (turret != null) {
+			Debug.Log ("Can't Build There! - TODO: Display On Screen.");
+			return;
+		}
+
+		GameObject turretToBuild = buildManager.GetTurretToBuild ();
+		turret = Instantiate (turretToBuild, transform.position + positionOffset, transform.rotation) as GameObject;
+
+
+	}
+
+	void OnMouseEnter () {
+
+		if (EventSystem.current.IsPointerOverGameObject ()) {
+			return;
+		}
+
+		if (buildManager.GetTurretToBuild () == null) {
+			return;
+		}
+		rend.material.color = hoverColor;
+	}
+
+	void OnMouseExit () {
+		rend.material.color = startColor;
+	}
+
+}
